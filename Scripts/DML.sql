adff704948157533cc75bbec35bc8bdb92d1df50
Vitor Luiz De Carvalho Gonçalves <45999057809@info.scs>
2022-02-24 16:22:59 -0300

    ScriptsEmTeste

---
+++ Scripts/DML.sql
@@ -0,0 +1,13 @@
+USE Classificados_2
+
+INSERT INTO TipoUsuarios(TipoUsuario) VALUES ('Admin'), ('Comum')
+GO
+
+INSERT INTO TipoClassificados(Tipo) VALUES ('Produto'),('Serviço'),('Imobiliária')
+GO
+
+INSERT INTO Nichos(Nicho) VALUES ('Tecnologia'),('Esportes'),('Esilo de Vida'),('Beleza'),('Saúde')
+GO
+
+INSERT INTO 
+
