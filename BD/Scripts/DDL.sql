22b377b697cfaf0449e4efba1ae2c49902432933
Vitor Luiz De Carvalho Gonçalves <45999057809@info.scs>
2022-02-25 13:39:09 -0300

    BancoRevisadoConcluido

--- BD/Scripts/DDL.sql
+++ BD/Scripts/DDL.sql
@@ -4,32 +4,32 @@ CREATE DATABASE Classificados_2
 
 CREATE TABLE TipoUsuarios(
 idTipoUsuario TINYINT PRIMARY KEY IDENTITY,
-TipoUsuario CHAR(5) UNIQUE NOT NULL,
+TipoUsuario CHAR(5) UNIQUE NOT NULL
 )
 GO
 
 CREATE TABLE TipoClassificados(
 idTipoClassificados TINYINT PRIMARY KEY IDENTITY,
-Tipo VARCHAR(16) UNIQUE NOT NULL,
+Tipo VARCHAR(16) UNIQUE NOT NULL
 )
 GO
 
 CREATE TABLE Nichos(
 idNicho TINYINT PRIMARY KEY IDENTITY,
-Nicho VARCHAR(25) UNIQUE NOT NULL,
+Nicho VARCHAR(25) UNIQUE NOT NULL
 )
 GO
 
 CREATE TABLE Categorias(
 idCategoria SMALLINT PRIMARY KEY IDENTITY,
 idNicho TINYINT FOREIGN KEY REFERENCES Nichos(idNicho),
-Categoria VARCHAR(30) UNIQUE NOT NULL,
+Categoria VARCHAR(30) UNIQUE NOT NULL
 )
 GO
 
 CREATE TABLE Situacao(
 idSituacao TINYINT PRIMARY KEY IDENTITY,
-Situacao VARCHAR(15) UNIQUE NOT NULL,
+Situacao VARCHAR(15) UNIQUE NOT NULL
 )
 GO
 
@@ -39,27 +39,35 @@ idTipoUsuario TINYINT FOREIGN KEY REFERENCES TipoUsuarios(idTipoUsuario),
 Nome VARCHAR(20),
 Sobrenome VARCHAR(50),
 Email VARCHAR(256) UNIQUE,
-Senha CHAR(32) NOT NULL,
+Senha CHAR(60) NOT NULL,
 DDD VARCHAR(3),
-Telefone VARCHAR(9) UNIQUE,
+Telefone VARCHAR(9) UNIQUE
 )
 
 CREATE TABLE Classificados(
 idClassificado INT PRIMARY KEY IDENTITY,
 idUsuario INT FOREIGN KEY REFERENCES Usuarios(idUsuario),
 idSituacao TINYINT FOREIGN KEY REFERENCES Situacao(idSituacao),
-Titulo VARCHAR(50) NOT NULL,
+Titulo VARCHAR(100) NOT NULL,
 Descricao VARCHAR(300),
 DataCricao DATETIME,
 DataExpiracao DATETIME,
-Acessos SMALLINT,
+Acessos SMALLINT
 )
 
 CREATE TABLE Ofertas(
 idOfertas INT PRIMARY KEY IDENTITY,
 idUsuario INT FOREIGN KEY REFERENCES Usuarios(idUsuario),
 idClassificado INT FOREIGN KEY REFERENCES Classificados(idClassificado),
-idSituacao TINYINT FOREIGN KEY REFERENCES Situacao(idSituacao),
+idSituacao TINYINT FOREIGN KEY REFERENCES Situacao(idSituacao)
 )
 
-
+CREATE TABLE ImagemBanco (
+    idImg INT PRIMARY KEY identity(1,1),
+    idUsuario INT FOREIGN KEY REFERENCES Usuarios(idUsuario),
+	idClassificado INT FOREIGN KEY REFERENCES Classificados(idclassificado),
+    Binario VARBINARY(MAX) NOT NULL,
+    mimeType VARCHAR(30) NOT NULL,
+    NomeArquivo VARCHAR(150) NOT NULL
+);
+GO
--- BD/Scripts/DML.sql
+++ BD/Scripts/DML.sql
@@ -9,5 +9,11 @@ GO
 INSERT INTO Nichos(Nicho) VALUES ('Tecnologia'),('Esportes'),('Esilo de Vida'),('Beleza'),('Saúde')
 GO
 
-INSERT INTO 
+INSERT INTO Categorias VALUES (1,'Computadores'),(1,'Notebooks'),(1,'Video-Games'),(3,'Hair Style'),(4,'Maquiagem'),(5,'Equipamentos Aeróbicos')
+GO
 
+INSERT INTO Situacao VALUES ('Em Andamento'),('Cancelado'),('Concluida'),('Negada'),('Aceita'),('Favoritada')
+GO
+
+INSERT INTO Usuarios(idTipoUsuario,Nome,Email,Senha) VALUES (1,'Administrador','admin@admin','$2y$11$AMq74F4glA44Td4Gskj4v.49M7kpKcv3MeUwjZi83BEbwl6ZQ0uB2')
+GO
