e9c699ecdffd33dda449e48c7b2d15d1e7127606
Zeck <61870100+z-eck@users.noreply.github.com>
2022-03-07 11:38:31 -0300

    adicionando o jsx dos meus classificados
    perdão a todos, mas eu sou terrível em css, eu juro que até o final desse curso, eu melhoro

--- FrontEnd/WEB/classificados/src/Pages/meusClassificados/meusClassificados.jsx
+++ FrontEnd/WEB/classificados/src/Pages/meusClassificados/meusClassificados.jsx
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { Component } from 'react';
 
+import '../../Assets/css/meusClassificados.css'
+
+import Header from "../../Components/header.jsx"
+
 export default class MeusClassificados extends Component {
     constructor(props) {
         super(props);
@@ -12,7 +16,7 @@ export default class MeusClassificados extends Component {
 
     listarMeusClassificados = () => {
 
-        axios('http://localhost:5000/api/Classificado/meus', {
+        axios('http://localhost:5000/api/Classificado/ListarMinhas', {
             headers: {
                 'Authorization': 'Bearer ' + localStorage.getItem('usuario-login')
             }
@@ -22,7 +26,7 @@ export default class MeusClassificados extends Component {
                     this.setState({ listaClassificados: response.data })
                 }
             })
-            .catch( erro => console.log(erro));
+            .catch(erro => console.log(erro));
     }
 
     // anunciarClassificado = () => {
@@ -31,6 +35,55 @@ export default class MeusClassificados extends Component {
     //     axios.post('http://localhost:5000/api/Classificado')
     // }
 
-    // Precisa saber onde os campos de cadastroClassificados ficará
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="body">
+                    <div className="tituloSecao">
+                        <h4>MEUS ANÚNCIOS</h4>
+                        <button className="btnAnunciar" type="submit"><h5>Anunciar</h5></button>
+                    </div>
+                    <div className="listaDosClassificados">
+                        {/* {this.listarMeusClassificados.map((meusClassificados) => {
+                            return ( */}
+                                <div className="classificadoUnico">
+                                    <div className="imgClassif">
+                                        {/* {meusClassificados.Imagem} */}
+                                    </div>
+                                    <div className='infoClassif'>
+                                        <div className='infoClassifTeD'>
+                                            <div className="tituloClassif">
+                                                <h6>Alugue a Lua por sei lá mano, to cansado desse trabalho, só faz ele</h6>
+                                                <h6>
+                                                    {/* {meusClassificados.Titulo} */}
+                                                    </h6>
+                                            </div>
+                                            <div className='linhaClassif' />
+                                            <div className="descClassif">
+                                                <p>Já imaginou ter um cruzeiro de ida lunar? Não? Pois bem, te alugo um pedaço da Lua para você ter essa experiência!</p>
+                                                {/* <p>{meusClassificados.Descricao}</p> */}
+                                            </div>
+                                        </div>
+                                        <div className="oferta___tempo">
+                                            <div className="ofertaFavorita">
+                                                <h6>300 Centavos</h6>
+                                                {/* <h6>{meusClassificados.idOfertasNavigation.Descricao}</h6> */}
+                                            </div>
+                                            <div className="tempoClass">
+                                                <h5>99:59:59</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* )
+                            })} */}
+                    </div>
+
+                    
+                </div>
+            </div>
+        )
+    }
 
 }
